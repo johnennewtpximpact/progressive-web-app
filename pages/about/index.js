99019d4c21744a598e2695b625f0dfd8c550cb9b
John Ennew <john.ennew@manifesto.co.uk>
2022-01-25 15:40:57 +0000

    Added an about page

---
+++ pages/about/index.js
@@ -0,0 +1,24 @@
+import styles from '../../styles/Home.module.css'
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          About <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
