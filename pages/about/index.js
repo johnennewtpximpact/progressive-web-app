c4e51153eab255ee27557e1e6f0ec920be0b08fb
John Ennew <john.ennew@manifesto.co.uk>
2022-01-25 15:44:07 +0000

    Added some links like a boss

--- pages/about/index.js
+++ pages/about/index.js
@@ -7,6 +7,37 @@ export default function About() {
         <h1 className={styles.title}>
           About <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+          <div className={styles.grid}>
+              <a href="/" className={styles.card}>
+                  <h3>Home &rarr;</h3>
+                  <p>Lets go right back home.</p>
+              </a>
+
+              <a href="https://nextjs.org/learn" className={styles.card}>
+                  <h3>Learn &rarr;</h3>
+                  <p>Learn about Next.js in an interactive course with quizzes!</p>
+              </a>
+
+              <a
+                  href="https://github.com/vercel/next.js/tree/canary/examples"
+                  className={styles.card}
+              >
+                  <h3>Examples &rarr;</h3>
+                  <p>Discover and deploy boilerplate example Next.js projects.</p>
+              </a>
+
+              <a
+                  href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                  className={styles.card}
+              >
+                  <h3>Deploy &rarr;</h3>
+                  <p>
+                      Instantly deploy your Next.js site to a public URL with Vercel.
+                  </p>
+              </a>
+          </div>
+
       </main>
 
       <footer className={styles.footer}>
--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/about" className={styles.card}>
+            <h3>About &rarr;</h3>
+            <p>Lets go to another page and see if it caches like magic.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
